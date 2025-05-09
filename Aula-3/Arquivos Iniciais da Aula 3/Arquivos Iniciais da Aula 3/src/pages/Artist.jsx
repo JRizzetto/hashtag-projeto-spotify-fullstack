236f0a4a8e56e1d4de5b0a725adf623808d33bcc
jeffersonrizzetto@gmail.com <jeffersonrizzetto@gmail.com>
2025-05-09 15:13:39 -0300

    Projeto Spotify finalizado

---
+++ Aula-3/Arquivos Iniciais da Aula 3/Arquivos Iniciais da Aula 3/src/pages/Artist.jsx
@@ -0,0 +1,28 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCirclePlay } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+
+const Artist = () => {
+  return (
+    <div className="artist">
+      <div
+        className="artist__header"
+        style={{
+          backgroundImage:
+            "linear-gradient(to bottom, var(--_shade), var(--_shade)),url(https://i.scdn.co/image/ab67618600001016909a162796dbc3629a1fadeb)",
+        }}
+      ></div>
+
+      <div className="artist__body"></div>
+
+      <Link to="/song/1">
+        <FontAwesomeIcon
+          className="single-item__icon single-item__icon--artist"
+          icon={faCirclePlay}
+        />
+      </Link>
+    </div>
+  );
+};
+
+export default Artist;
